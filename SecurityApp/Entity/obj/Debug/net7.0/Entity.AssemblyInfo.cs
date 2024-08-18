1b8942b88a0e03659297fbc6962cbc3239064f92
Daniel7033 <j.audor7033@gmail.com>
2024-08-17 23:21:41 -0500

    Correccion MER proyecto

--- SecurityApp/Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
+++ SecurityApp/Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176aa089017f65d042abad4d4af26beadc42ce47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
