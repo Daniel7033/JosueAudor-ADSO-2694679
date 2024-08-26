1c3e17118c9671da7720c1c46f42c6d7b3ca1f13
Daniel7033 <j.audor7033@gmail.com>
2024-08-25 19:44:11 -0500

    angular

--- SecurityApp/Business/obj/Debug/net7.0/Business.AssemblyInfo.cs
+++ SecurityApp/Business/obj/Debug/net7.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176aa089017f65d042abad4d4af26beadc42ce47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8942b88a0e03659297fbc6962cbc3239064f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
